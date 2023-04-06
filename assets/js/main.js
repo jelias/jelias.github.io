8af70ac92efeceeff664c748008a78e877ba003f
Jacob Elias <jelias@users.noreply.github.com>
2023-04-06 09:39:34 +0000

    Started building system dark mode and updated linting

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const runCheckColorMode = (fn) => {
+  if (!window.matchMedia) {
+    return;
+  }
+
+  const query = window.matchMedia("(prefers-color-scheme: dark)");
+
+  fn(query.matches);
+
+  query.addEventListener("change", (event) => fn(event.matches));
+};
+
+runCheckColorMode((isDarkMode) => {
+  if (isDarkMode) {
+    document.documentElement.classList.add("dark-mode");
+    console.log("dark mode!");
+  } else {
+    document.documentElement.classList.remove("dark-mode");
+  }
+});
